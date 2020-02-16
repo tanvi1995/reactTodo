839a7776778d999b5ff0198c3b3a61d8a875a118
tanvi1995 <45558205+tanvi1995@users.noreply.github.com>
2020-02-16 15:56:52 +0530

    Update reducer.js

--- store/reducer/reducer.js
+++ store/reducer/reducer.js
@@ -1,61 +1,73 @@
-import * as actionTypes  from '../actions/action'
-import {combineReducers} from 'redux'
-import Users from '../../components/Users/Users';
+import * as actionTypes from "../actions/action";
 
 const initialState = {
-    todos: 
-    [
-        {name: 'Joy Brown',email: 'joy12@gmail.com',id: '1'},
-        {name: 'Adam Gren',email: 'adam13@gmail.com',id: '2'},
-        {name: 'Daniel Black',email: 'daniel11@gmail.com',id: '3'},
-    ],
-    users: 
-    [
-        {name: 'John Mull',email: 'john12@gmail.com',id: '7'},
-        {name: 'Tom Cruz',email: 'tom11@gmail.com',id: '8'}, 
-        {name: 'Mary Turner',email: 'mary13@gmail.com',id: '9'}
-    ]
-}
-
-
-const reducer = (state=initialState, action) =>{
-    
-    console.log("reducer",state);
-    switch(action.type){
-        case actionTypes.LIST_TODOS:
-            return{
-                ...state,
-                todos:{...state.todos}
-            };
-        case actionTypes.LIST_USERS:
-            return{
-                ...state,
-                users:{...state.users}
-            };
-        case actionTypes.ADD_USER:
-            state.unshift(action.payload);
-            return [...state, action.payload];
-        
-        case actionTypes.DELETE_USER:
-        {
-            console.log(state.users)
-            state.users.find(user=>{
-                console.log(user.name)
-            })
-            // state= state.slice();
-            // state.splice(action.payload,1)
-            //return state = state.map(s => Users(s, action))
-            // ...state,
-            // users:{...state.users}
-        };
-        case actionTypes.EDIT_USER:
-        return{
-            ...state,
-            users:{...state.users}
-        };
-        default:
-            return state;
+  todos: [
+    {
+      name: "Joy Brown",
+      email: "joy12@gmail.com",
+      timestampDue: 1561881986756
+    },
+    {
+      name: "Adam Gren",
+      email: "adam13@gmail.com",
+      timestampDue: 1233456789016
+    },
+    {
+      name: "Daniel Black",
+      email: "daniel11@gmail.com",
+      timestampDue: 1234252562627
     }
-    
-}
-export default reducer
+  ],
+  users: [
+    { name: "John Mull", email: "john12@gmail.com", id: "7" },
+    { name: "Tom Cruz", email: "tom11@gmail.com", id: "8" },
+    { name: "Mary Turner", email: "mary13@gmail.com", id: "9" }
+  ]
+};
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case actionTypes.LIST_TODOS:
+      return {
+        ...state,
+        todos: { ...state.todos }
+      };
+    case actionTypes.DELETE_TODO:
+      var todos = state.todos.filter(value => value.email !== action.payload);
+      return {
+        ...state,
+        todos
+      };
+    case actionTypes.LIST_USERS:
+      return {
+        ...state,
+        users: { ...state.users }
+      };
+    case actionTypes.ADD_USER:
+      return {
+        ...state,
+        users: [...state.users, action.payload]
+      };
+
+    case actionTypes.DELETE_USER:
+      var users = state.users.filter(value => value.email !== action.payload);
+      return {
+        ...state,
+        users
+      };
+    case actionTypes.EDIT_USER:
+      users = state.users.map(value => {
+        if (value.email === action.payload.email) {
+          return action.payload;
+        } else {
+          return value;
+        }
+      });
+      return {
+        ...state,
+        users
+      };
+    default:
+      return state;
+  }
+};
+export default reducer;
