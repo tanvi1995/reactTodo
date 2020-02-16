5c2008bcb48a887d2e43e154938c5b2d6fea9ef2
tanvi1995 <45558205+tanvi1995@users.noreply.github.com>
2020-02-16 15:55:20 +0530

    Update Users.js

--- components/Users/Users.js
+++ components/Users/Users.js
@@ -1,77 +1,88 @@
-import React,{Component} from 'react'
-import { bindActionCreators } from 'redux';
-import '../Users/userList.css'
-import { Icon } from 'antd';
-import * as action  from '../../store/actions/index'
-import { deleteUser } from  '../../store/actions/action'
-import {connect} from 'react-redux'
-//import ReactDOM from 'react-dom'
+import React, { Component } from "react";
+import "../Users/userList.css";
+import { Icon } from "antd";
+import * as action from "../../store/actions/index";
+import { connect } from "react-redux";
+import EditModal from "../Modal/EditModal";
+import Createuser from "../Createuser/Createuser";
 
-// const Users = ({users}) => {
-// console.log('users',users)
-//   const userList = users.length ? (
-//     users.map(user => {
-//       return (
-//         <div className="todo-col" key={user.name}>
-//           <span>{user.name}</span>
-//           <span>{user.email}</span>
-//         </div>
-//       )
-//     })
-//   ) : (
-//     <p>You have no users</p>
-//   )
-//   return <div className="todo-cl">{userList}</div>
-// }
-class Users extends Component{
-  
-  // deleteUser = (user) => {
-  //   this.props.actions.DeleteUser(user)
-    
-  // }
-  
-  render(){
-    console.log("users.js",this.props.users)
-  return(
-    <div className="userListMain">
-      {this.props.users? 
-        this.props.users.map(user=>{
-            return <span key={user.id}>
-              <ul>
-                <li>{user.name}
-                  {/* <span className="editIcon" onClick="editTodo">Edit<Icon className="icon" type="edit" /></span> */}
-                  <span className="deleteIcon" onClick={()=>this.props.onUserDelete(user.name)} >Delete<Icon type="delete" /></span>
-                </li>
-              </ul>
-            </span>
-          })
-        : "No Users"
-      }
-    </div>
-  )
+class Users extends Component {
+  constructor() {
+    super();
+    this.state = {
+      isOpen: false
+    };
+  }
+  openModalHandler = email => {
+    this.setState({
+      editEmail: email,
+      isOpen: true
+    });
+  };
+  closeModalHandler = () => {
+    this.setState({
+      isOpen: false
+    });
+  };
+  render() {
+    return (
+      <div className="userListMain">
+        <Createuser onClick={user => console.log("app", user)} />
+        <br />
+        {this.props.users
+          ? this.props.users.map((user, index) => {
+              return (
+                <span key={index}>
+                  <ul>
+                    <li>
+                      {user.name}
+                      <span
+                        className="editIcon"
+                        onClick={e => this.openModalHandler(user.email)}
+                      >
+                        Edit
+                        <Icon className="icon" type="edit" />
+                      </span>
+                      {this.state.isOpen &&
+                      this.state.editEmail === user.email ? (
+                        <EditModal
+                          className="modal"
+                          user={user}
+                          show={this.state.isOpen}
+                          close={this.closeModalHandler}
+                        />
+                      ) : (
+                        ""
+                      )}
+                      <span
+                        className="deleteIcon"
+                        onClick={() => this.props.deleteUser(user.email)}
+                      >
+                        Delete
+                        <Icon type="delete" />
+                      </span>
+                    </li>
+                  </ul>
+                </span>
+              );
+            })
+          : "No Users"}
+      </div>
+    );
+  }
 }
-}
-const mapStateToProps = state =>{
-  return{
+const mapStateToProps = state => {
+  return {
     users: state.users
-  }
-}
-const mapDispatchToProps = dispatch =>{
-  return{
-    onUserDelete: (name) => dispatch(action.deleteUser(name))
-  }
-}
-// function mapStateToProps(state, ownProps) {
-//   return {
-//       users: state.users
-//   }
-// }
+  };
+};
+const mapDispatchToProps = dispatch => {
+  return {
+    deleteUser: name => dispatch(action.deleteUser(name))
+  };
+};
 
-// // This maps the dispatch to the property of the component
-
-// function mapDispatchToProps(dispatch) {
-//   return {
-//       actions: bindActionCreators(action, dispatch)
-//   }
-// }
-export default connect(mapStateToProps,mapDispatchToProps)(Users)
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Users);
